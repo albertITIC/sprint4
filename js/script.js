b09a2b5d470345068dce6b694dc230b0a1d43279
Albert <albertpencas@gmail.com>
2024-12-16 23:39:28 +0100

    Professors fets

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Apartat professor
+document.addEventListener("DOMContentLoaded", () => {
+    // Capturar eventos de los botones
+    const editarBotones = document.querySelectorAll(".btn-editar");
+    const eliminarBotones = document.querySelectorAll(".btn-eliminar");
+
+    editarBotones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            alert("Editar l'usuari.")
+        });
+    });
+
+    eliminarBotones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            alert("Eliminar usuari.");
+        });
+    });
+});
+
+// Apartat alumne
+document.addEventListener("DOMContentLoaded", () => {
+    // Acciones para botones de módulos
+    const modulos = document.querySelectorAll(".modul button");
+
+    modulos.forEach(boton => {
+        boton.addEventListener("click", (e) => {
+            const moduloId = e.target.parentElement.id;
+            alert(`Has seleccionat el ${moduloId.toUpperCase()}!`);
+            // Aquí puedes agregar más lógica para cada módulo.
+        });
+    });
+
+    // Capturar eventos de botones del profesor
+    const editarBotones = document.querySelectorAll(".btn-editar");
+    const eliminarBotones = document.querySelectorAll(".btn-eliminar");
+
+    editarBotones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            alert("Funcionalitat per editar l'usuari encara no implementada.");
+        });
+    });
+
+    eliminarBotones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            alert("Funcionalitat per eliminar l'usuari encara no implementada.");
+        });
+    });
+});
